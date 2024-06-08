0be7eeaca5325f66c6fd5f19d193927ab0f32206
Presotn Brubaker <brubakerpreston@gmail.com>
2024-06-08 10:35:21 -0500

    happy day

--- nn.py
+++ nn.py
@@ -12,22 +12,55 @@ test_points = 50
 
 best_fitness = 10000000
 
-while True:
-    genes_m = []
-    for i in range(0, len(genes)):  # Cycles through the genes to create a mutant
-        a = genes[i][0]
+def test_agent(genes_in, state_in):
+    fitness_a = 0
+    with open('data.txt', 'w') as file:
+        file.write("")
+    with open('gene.txt', 'w') as file:
+        file.write(str(genes_in))
+    for k in range(0, test_points):  # Tests the agent with various inputs and outputs
+        x = k / test_points
+        y = x ** 2
+        for j in range(0, len(state_in)):
+            state_in[j] = 0
+        state_in[0] = x
+        for i in range(0, len(genes_in)):  # Cycles through the genes to execute the instructions
+            a = genes_in[i][0]
+            b = genes_in[i][1]
+            transfer_mode = genes_in[i][2]
+            magnitude = genes_in[i][3]
+            if transfer_mode == 0:
+                if state_in[a] > 0:
+                    state_in[b] += magnitude
+                    state_in[a] -= magnitude
+            elif transfer_mode == 1:
+                if state_in[a] > 0:
+                    state_in[b] += magnitude * state_in[a]
+                    state_in[a] -= magnitude * state_in[a]
+            elif transfer_mode == 2:
+                state_in[b] += magnitude
+        result = state_in[-1]
+        with open('data.txt', 'a') as file:
+            file.write(str(x) + " " + str(y) + " " + str(result) + "\n")
+        fitness_a += (y - result) ** 2 / (test_points + 1) * 10000
+    return fitness_a
+
+def mutate_agent(genes_in):
+    genes_out = []
+    for i in range(0, len(genes_in)):  # Cycles through the genes to create a mutant
+        a = genes_in[i][0]
         r = random.uniform(0, 1)
         if r < mut_c:
             a = random.randint(0, len(state) - 1)
-        b = genes[i][1]
+        b = genes_in[i][1]
         r = random.uniform(0, 1)
         if r < mut_c:
             b = random.randint(0, len(state) - 1)
-        transfer_mode = genes[i][2]
+        transfer_mode = genes_in[i][2]
         r = random.uniform(0, 1)
         if r < mut_c:
             transfer_mode = random.randint(0, 2)
-        magnitude = genes[i][3]
+        magnitude = genes_in[i][3]
         r = random.uniform(0, 1)
         if r < mut_c:
             magnitude = random.uniform(-1, 1) * 10 ** random.randint(-6, 1)
@@ -35,38 +68,18 @@ while True:
                 magnitude = -1
             if magnitude > 2:
                 magnitude = 1
-        genes_m.append([a, b, transfer_mode, magnitude])
+        genes_out.append([a, b, transfer_mode, magnitude])
+    return genes_out
 
-    fitness = 0
-    with open('data.txt', 'w') as file:
-        file.write("")
-    with open('gene.txt', 'w') as file:
-        file.write(str(genes))
-    for k in range(0, test_points):  # Tests the agent with various inputs and outputs
-        x = k / test_points
-        y = x ** 2
-        for j in range(0, len(state)):
-            state[j] = 0
-        state[0] = x
-        for i in range(0, len(genes)):  # Cycles through the genes to execute the instructions
-            a = genes_m[i][0]
-            b = genes_m[i][1]
-            transfer_mode = genes_m[i][2]
-            magnitude = genes_m[i][3]
-            if transfer_mode == 0:
-                if state[a] > 0:
-                    state[b] += magnitude
-                    state[a] -= magnitude
-            elif transfer_mode == 1:
-                if state[a] > 0:
-                    state[b] += magnitude * state[a]
-                    state[a] -= magnitude * state[a]
-            elif transfer_mode == 2:
-                state[b] += magnitude
-        result = state[-1]
-        with open('data.txt', 'a') as file:
-            file.write(str(x) + " " + str(y) + " " + str(result) + "\n")
-        fitness += (y - result) ** 2 / (test_points + 1) * 10000
+
+while True:
+    genes_m = mutate_agent(genes)
+    
+
+
+    fitness = test_agent(genes_m, state)
+
+    
     if fitness <= best_fitness:
         best_fitness = fitness
         print("Iteration: " + str(it_C) + " Fitness: " + str(fitness))
